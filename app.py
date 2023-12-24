4bc95fe37489efec80eb05133bf12b6ae52b93ac
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2023-12-23 21:38:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import speech_recognition as sr
 import streamlit as st
 import pybraille
+import pyaudio
 
 TIME = 5
 
-def recognize_speech_from_file(file_path):
-    r = sr.Recognizer()
-    with sr.AudioFile(file_path) as source:
-        print("Processing audio file...")
-        try:
-            audio = r.record(source)
-            print("Audio file processed.")
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-        except sr.RequestError as e:
-            print(f"Could not request results from Google Speech Recognition service; {e}")
-        except sr.UnknownValueError:
-            print("Sorry, could not understand audio.")
-        else:
-            return r.recognize_google(audio)
+
+def recognize_speech():
+  r = sr.Recognizer()
+  with sr.Microphone() as source:
+    print("Say something!")
+    try:
+        audio = r.listen(source, timeout=TIME)
+        print("Recording completed.")
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.WaitTimeoutError:
+        print("Speech recognition timed out. No speech detected in 5 seconds.")
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
+    except sr.UnknownValueError:
+        print("Sorry, could not understand audio.")
+    else:
+        return r.recognize_google(audio)
+
+
 
 def word_to_braille(text):
     converted_phrase = []
@@ -26,13 +32,13 @@ def word_to_braille(text):
         converted_phrase.append(braille_instructions)
     return converted_phrase
 
-if st.button("Recognize from Audio File"):
-    uploaded_file = st.file_uploader("Choose an audio file", type=["mp3"])
-    if uploaded_file is not None:
-        text = recognize_speech_from_file("uploaded_file")
-        print("We translated:", text)
 
+if st.button("Speak"):
+  text = recognize_speech()
+  print("We translated:", text)
+  
 if st.button("Convert to Braille"):
     text = text.strip()
     braille_instructions = word_to_braille(text)
-    print(f"Braille instructions for ''{text}'' are: {braille_instructions}")
+    print(f"Braille instructions for ''{word}'' are: {braille_instructions}")
+    
