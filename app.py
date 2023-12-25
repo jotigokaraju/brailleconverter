1a776a5e8b856c8ba17745f957aeddc4a2df989f
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2023-12-25 13:45:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,13 @@ state=st.session_state
 
 def recognize_speech():
   
-    if 'text_received' not in state:
-        state.text_received=[]
-
     c1,c2=st.columns(2)
     with c1:
         st.write("Convert speech to text:")
     with c2:
         text=speech_to_text(language='en',use_container_width=True,just_once=True,key='STT')
-        st.write(text)
-      
-    if text:       
-        state.text_received.append(text)
-
-    for text in state.text_received:
-        st.text(text)
-    
-    return text
+        st.write(text)   
+        return text
 
 
 def word_to_braille(text):
