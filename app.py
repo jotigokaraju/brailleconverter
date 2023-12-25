df9a3455066b2d458e5ffd6013f2c141ba8349c1
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2023-12-25 13:51:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,21 @@ from streamlit_mic_recorder import mic_recorder,speech_to_text
 TIME = 5
 state=st.session_state
 
-def recognize_speech():
-  
-    if 'text_received' not in state:
-        state.text_received=[]
 
-    c1,c2=st.columns(2)
-    with c1:
-        st.write("Convert speech to text:")
-    with c2:
-        text=speech_to_text(language='en',use_container_width=True,just_once=True,key='STT')
-    
-    if text:       
-        state.text_received.append(text)
-    
-    for text in state.text_received:
-        st.text(text)
+if 'text_received' not in state:
+    state.text_received=[]
+
+c1,c2=st.columns(2)
+with c1:
+    st.write("Convert speech to text:")
+with c2:
+    text=speech_to_text(language='en',use_container_width=True,just_once=True,key='STT')
+
+if text:       
+    state.text_received.append(text)
+
+for text in state.text_received:
+    st.text(text)
 
 '''
 def word_to_braille(text):
@@ -31,8 +30,8 @@ def word_to_braille(text):
     return converted_phrase
 '''
 
-if st.button("Speak"):
-  recognize_speech()
+#if st.button("Speak"):
+  #recognize_speech()
   #print(state.recognized_text)
   #print("We translated:", text)
 '''
