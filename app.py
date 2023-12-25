3a07d7d1a6cc9d8d668dca50e57671795dde904c
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2023-12-25 15:24:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,35 @@ word = []
 if 'text_received' not in state:
     state.text_received = []
 
+# Set page configuration
+st.set_page_config(
+    page_title="Translate Live Text to Braille",
+    page_icon="🔍",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+# Define colors
+light_blue = "#ADD8E6"
+divider_color = "#A9A9A9"
+
+# Define styles
+header_style = f"background-color: {light_blue}; padding: 1rem;"
+footer_style = "text-align: center; padding: 1rem;"
+divider_style = f"background-color: {divider_color}; height: 2px; margin: 1rem 0;"
+
 # Create columns for layout
 c1, c2 = st.columns(2)
 
+# Apply styles to the header
+st.markdown(
+    f'<div style="color: black; {header_style}">'
+    '<h1 style="margin: 0;">Translate Live Text to Braille</h1></div>',
+    unsafe_allow_html=True,
+)
+
 # Column 1: Display recorder and translation
 with c1:
-    st.title("Speech-to-Braille Converter")
     st.write("Record your speech and convert it to Braille!")
 
     # Speech-to-text recorder
@@ -32,7 +55,6 @@ with c1:
 
 # Column 2: Braille conversion
 with c2:
-    st.title("Braille Conversion")
     st.write("Convert translated text to Braille.")
 
     # Convert to Braille button
@@ -50,6 +72,13 @@ with c2:
             braille_instructions = word_to_braille(state.text_received)
             st.write(f"Braille instructions for {state.text_received} are: {braille_instructions}")
 
-# Additional formatting
-st.markdown("---")  # Horizontal divider
+# Additional styling
+st.markdown(divider_style, unsafe_allow_html=True)  # Horizontal divider
 st.write("Your custom content or additional information can go here.")
+
+# Footer
+st.markdown(divider_style, unsafe_allow_html=True)  # Divider above footer
+st.markdown(
+    f'<div style="color: black; {footer_style}">by Joti Gokaraju</div>',
+    unsafe_allow_html=True,
+)
