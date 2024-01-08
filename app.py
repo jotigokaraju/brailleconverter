7daf969c6383a20971e13347b2c6a2e1e23c4840
jotigokaraju <148422591+jotigokaraju@users.noreply.github.com>
2024-01-07 20:05:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def braille_to_instructions(commands):
 # Title Formatting with banner blue background
 st.title("TouchTalk")
 
-with st.expander("<h2>A Comprehensive Speech to Braille Platform</h2>"):
+with st.expander("<h2>A Comprehensive Speech to Braille Platform</h2>", raw=True):
     st.markdown("""
         This app is designed to assist the DeafBlind community, a traditionally underserved demographic. 
         The app translates live speech into Braille instructions that are read out by the TouchTalk device.
